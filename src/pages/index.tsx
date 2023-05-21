696bf803113632f851788b7067fa384767805fff
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-05-21 00:35:18 -0300

    home(feat): Criando caroussel de produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,42 @@ import camiseta2 from '../assets/camisetas/Camisa-Maratona 2.png'
 import camiseta3 from '../assets/camisetas/IgniteLab-T-shirt 2.png'
 import { MainContainer, Product } from "@/styles/pages/home";
 
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
 export default function Home() {
+  const [slideRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <MainContainer>
-      <Product href="">
+    <MainContainer ref={slideRef} className='keen-slider'>
+      <Product href="" className='keen-slider__slide'>
         <Image src={camiseta1} alt=""/>
         <footer>
           <span> Camiseta Beyhond</span>
           <strong> R$ 79,90 </strong>
         </footer>
       </Product>
-      <Product href="">
+      <Product href="" className='keen-slider__slide'>
         <Image src={camiseta2} alt=""/>
+        <footer>
+          <span> Camiseta Beyhond</span>
+          <strong> R$ 79,90 </strong>
+        </footer>
+      </Product >
+      <Product href="" className='keen-slider__slide'>
+        <Image src={camiseta3} alt=""/>
+        <footer>
+          <span> Camiseta Beyhond</span>
+          <strong> R$ 79,90 </strong>
+        </footer>
+      </Product>
+      <Product href="" className='keen-slider__slide'>
+        <Image src={camiseta3} alt=""/>
         <footer>
           <span> Camiseta Beyhond</span>
           <strong> R$ 79,90 </strong>
