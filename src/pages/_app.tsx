bcd8ae2d45672dffb76b66988de5911e673df801
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-05-21 00:46:59 -0300

    app(feat): setinha do caroussel criada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import igShop from '../assets/igShop.svg'
 import Image from 'next/image';
+import { ArrowRight } from "phosphor-react";
 import { Container, Header } from '@/styles/pages/app';
 
 globalStyle()
@@ -12,6 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Header>
         <Image src={igShop} alt="Logo" />
       </Header>
+      <div>
+        <ArrowRight size={32} color="white"/>
+      </div>
       <Component {...pageProps} />
     </Container>
   );
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -5,7 +5,18 @@ export const Container = styled('div', {
      flexDirection: 'column',
      justifyContent: 'center',
      alignItems: 'flex-start',
-     height: '100vh'
+     height: '100vh',
+
+     div: {
+        height: '100%',
+        padding: '2rem',
+        background: 'linear-gradient(90deg, transparent, #121214)',
+        position: 'absolute',
+        right: '0',
+        display: 'flex',
+        alignItems: 'center',
+        'z-index': '999'
+    }
 })
 
 export const Header = styled('header', {
