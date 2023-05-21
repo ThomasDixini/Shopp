cab39c1e19196fac96b9d80863664362613c83db
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-05-21 00:21:02 -0300

    home(feat): Estilizando a home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import igShop from '../assets/igShop.svg'
+import Image from 'next/image';
+import { Container, Header } from '@/styles/pages/app';
 
 globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={igShop} alt="Logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+
+import camiseta1 from '../assets/camisetas/2_explorer-t-shirt 2.png'
+import camiseta2 from '../assets/camisetas/Camisa-Maratona 2.png'
+import camiseta3 from '../assets/camisetas/IgniteLab-T-shirt 2.png'
+import { MainContainer, Product } from "@/styles/pages/home";
+
 export default function Home() {
   return (
-    <>
-      <h1> Hello World </h1>
-    </>
+    <MainContainer>
+      <Product href="">
+        <Image src={camiseta1} alt=""/>
+        <footer>
+          <span> Camiseta Beyhond</span>
+          <strong> R$ 79,90 </strong>
+        </footer>
+      </Product>
+      <Product href="">
+        <Image src={camiseta2} alt=""/>
+        <footer>
+          <span> Camiseta Beyhond</span>
+          <strong> R$ 79,90 </strong>
+        </footer>
+      </Product>
+    </MainContainer>
   )
 }
