5318831ac2495892c6d9830cf68dc4762d0565cf
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-05-25 22:41:42 -0300

    SuccessPage(feat): Dados ao efetuar compra carregados

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const { priceId } = req.body;
 
-    const successUrl = `${process.env.NEXT_URL}/success`
+    const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
     const cancelUrl = `${process.env.NEXT_URL}/`
 
     const checkoutSession = await stripe.checkout.sessions.create({
