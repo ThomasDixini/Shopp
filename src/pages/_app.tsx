4c5e5aa696f4014c642e51f2e3795b765588ac83
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2023-05-26 18:26:02 -0300

    Sacola(feat): SAcola de compras estruturada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,56 @@ import { globalStyle } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import igShop from '../assets/igShop.svg'
 import Image from 'next/image';
-import { ArrowRight } from "phosphor-react";
-import { Container, Header } from '@/styles/pages/app';
+import camiseta from '../assets/camisetas/2_explorer-t-shirt 2.png'
+import { AsideContainer, Container, Header, ProductContainer } from '@/styles/pages/app';
+import { useState } from 'react';
+import { Handbag } from 'phosphor-react';
 
 globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [sideBarOpen, setSideBarOpen] = useState(false);
+
   return (
     <Container>
       <Header>
         <Image src={igShop} alt="Logo" />
+        <button type="button">
+          <Handbag size={24} weight='bold'/>
+        </button>
       </Header>
       {/*<div>
         <ArrowRight size={32} color="white"/>
   </div>*/}
+      <AsideContainer>
+        <h1>Sacola de compras</h1>
+
+        <ProductContainer>
+          <div>
+            <Image src={camiseta} width={94} height={82} alt=""/>
+          </div>
+
+          <div>
+            <span>Camiseta Beyond the Limits</span>
+            <strong>R$ 79,90</strong>
+            <a> Remover </a>
+          </div>
+        </ProductContainer>
+
+        <div>
+          <div>
+            <span>Quantidade</span>
+            <span>3 items</span>
+          </div>
+          <div>
+            <strong>Valor total</strong>
+            <strong>R$ 278,00</strong>
+          </div>
+        </div>
+
+        <button type="submit">Finalizar compra</button>
+      </AsideContainer>
       <Component {...pageProps} />
     </Container>
   );
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -24,4 +24,91 @@ export const Header = styled('header', {
     width: '100%',
     maxWidth: 1100,
     margin: '0 auto',
+
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    button: {
+        padding: '0.825rem',
+        borderRadius: 6,
+        border: 0,
+        backgroundColor: '$gray800',
+        color: '$white',
+        '&:hover': {
+            filter: 'brightness(0.8)',
+        }
+    }
+})
+
+export const AsideContainer = styled('aside', {
+    position: 'absolute',
+    right: 0,
+    'z-index': 999,
+    minHeight: '100vh',
+    width: 480,
+    backgroundColor: '$gray800',
+
+    display: 'flex',
+    flexDirection: 'column',
+
+    padding: '3rem',
+
+    span: {
+        fontSize: '$md',
+        color: '$gray300',
+    },
+
+    'article + div': {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '0.5rem',
+
+        div: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontSize: '1.25rem'
+        }
+    },
+
+    button: {
+        padding: '1rem',
+        fontSize: '1.125rem',
+        fontWeight: 'bold',
+        color: '$white',
+        backgroundColor: '$green300',
+        border: 0,
+        borderRadius: 6,
+        marginTop: '2rem',
+
+        '&:hover': {
+            backgroundColor: '$green500'
+        }
+    }
+})
+export const ProductContainer = styled('article', {
+    display: 'flex',
+    gap: '1rem',
+    alignItems: 'center',
+    marginTop: '2rem',
+    marginBottom: 'auto',
+
+    'div:first-child': {
+        background: 'linear-gradient(#1EA483 0%, #7465D4 100%)',
+        borderRadius: 6,
+    },
+    'div:last-child': {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '0.5rem',
+        alignItems: 'flex-start'
+    },
+
+    a: {
+        display: 'block',
+        background: 'transparent',
+        border: 0,
+        fontWeight: 'bold',
+        color: '$green500',
+        cursor: 'pointer',
+    }
 })
